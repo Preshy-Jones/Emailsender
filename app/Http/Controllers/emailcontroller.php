5d0219f3e82275dd8871233d0026e1d5f5524fd5
Preshy-Jones <adedibuprecious@gmail.com>
2020-06-17 16:20:48 +0100

    email man

---
+++ app/Http/Controllers/emailcontroller.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class emailcontroller extends Controller
+{
+    public  function email(Request $request){
+        $email = $request->json()->all();
+        $details = [
+//            'title'=> 'Preshy Jones Rocks',
+            'body'=> $email['body'],
+            'sub' => $email['subject'],
+        ];
+        
+        \Mail::to($email['to'])->send(new\App\Mail\TestMail($details));
+        echo "email has been sent";
+    }
+}
---
+++ app/Mail/TestMail.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class TestMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $details;
+    public $sub;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($details)
+    {
+        $this->details = $details;
+//        $this->sub = $sub;
+    }
+
+    public function build()
+    {
+        return $this->subject($this->details['sub'])->view('emails.TestMail');        
+    }
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+}
