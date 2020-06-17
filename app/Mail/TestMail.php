189b53a6ad4bafc7153618971bd0766899220fde
Preshy-Jones <adedibuprecious@gmail.com>
2020-06-17 18:54:07 +0100

    blah

--- app/Mail/TestMail.php
+++ app/Mail/TestMail.php
@@ -27,7 +27,7 @@ class TestMail extends Mailable
 
     public function build()
     {
-        return $this->subject($this->details['sub'])->markdown('emails/Testmail');        
+        return $this->subject($this->details['sub'])->view('emails.TestMail');        
     }
     /**
      * Build the message.
