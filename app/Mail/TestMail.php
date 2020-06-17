9c04330af9d50039bc41d6fd64783c67a915012b
Preshy-Jones <adedibuprecious@gmail.com>
2020-06-17 18:00:47 +0100

    another commit

--- app/Mail/TestMail.php
+++ app/Mail/TestMail.php
@@ -27,7 +27,7 @@ class TestMail extends Mailable
 
     public function build()
     {
-        return $this->subject($this->details['sub'])->markdown('emails/TestMail');        
+        return $this->subject($this->details['sub'])->markdown('emails/TestMail.blade.php');        
     }
     /**
      * Build the message.
